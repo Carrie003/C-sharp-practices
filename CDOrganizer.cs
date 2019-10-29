70b64e584ece185b4d550846953bf05060e5de8a
Carrie003 <46972243+Carrie003@users.noreply.github.com>
2019-10-28 21:29:30 -0500

    Add files via upload

---
+++ CDOrganizer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+
+// created an interesting CD organizer which can organize and retreve  CDs by their categories 
+
+// class and method practices
+
+namespace CDOrganizer.Models
+{
+    public class Category
+    {
+        private static List<Category> _allCDs = new List<Category> { };
+        private string _name;
+        private int _categoryId;
+        private List<CD> _cds;
+
+
+        public Category(string categoryName)
+        {
+            _name = categoryName;
+            _allCDs.Add(this);
+            _categoryId = _allCDs.Count;
+            _cds = new List<CD> { };
+        }
+
+        public string GetCategoryName()
+        {
+            return _name;
+        }
+
+        public int GetCategoryId()
+        {
+            return _categoryId;
+        }
+
+        public static List<Category> GetAllCDs()
+        {
+            return _allCDs;
+        }
+
+        public List<CD> GetCDs()
+        {
+            return _cds;
+        }
+
+        public static Category Find(int searchId)
+        {
+            return _allCDs[searchId - 1];
+        }
+
+        public void AddCD(CD cd)
+        {
+            _cds.Add(cd);
+        }
+
+    }
+
+}
